87aa15866100a044bda3572ecbeca89c9720f201
Анастасия <shishlakovanastya@gmail.com>
2020-02-12 14:51:56 +0300

    add moving for filter popup

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,12 +1,15 @@
 (function() {
   const orderPopup = document.querySelector(".order-room");
   const closeToggle = document.querySelector(".order-room__toggle");
-  const buttons = document.querySelectorAll(".button");
+  const buttons = document.querySelectorAll(".button-reserve");
   const orderAnswer = document.querySelector(".order-answer");
   const closeToggleAnswer = document.querySelector(".order-answer__toggle");
   const orderButton = document.querySelector(".order-room__button");
   const orderButtonAnswer = document.querySelector(".order-answer__button");
   const inputs = document.querySelectorAll(".order-room__form input");
+  const filterButton = document.querySelector(".catalog__button-filter");
+  const catalogFilterWrapper = document.querySelector(".catalog__filters");
+  const closeFilters = document.querySelector(".catalog__filters-toggle");
 
   function closePopup(e) {
     e.preventDefault();
@@ -27,6 +30,14 @@
     e.preventDefault();
     orderAnswer.classList.toggle("hidden");
   }
+  function openFilterWrapper(e) {
+    e.preventDefault();
+    catalogFilterWrapper.classList.toggle("hidden");
+  }
+  function closeFilterWrapper(e) {
+    e.preventDefault();
+    catalogFilterWrapper.classList.toggle("hidden");
+  }
   closeToggle.addEventListener("click", closePopup);
 
   buttons.forEach(function(btn) {
@@ -35,4 +46,7 @@
   orderButton.addEventListener("click", openPopupAnswer, closePopup);
   closeToggleAnswer.addEventListener("click", closePopupAnswer);
   orderButtonAnswer.addEventListener("click", closePopupAnswer);
+
+  filterButton.addEventListener("click", openFilterWrapper);
+  closeFilters.addEventListener("click", closeFilterWrapper);
 })();
